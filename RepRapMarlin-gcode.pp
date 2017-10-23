eeec7cbb950c1840fea812e3f0c5e9e2597b3186
Alexandr <gav-sash0k@yandex.ru>
2017-10-23 23:13:05 +0300

    Create RepRapMarlin-gcode.pp

---
+++ RepRapMarlin-gcode.pp
@@ -0,0 +1,136 @@
++================================================
++                                                
++               REPRAP Marlin G-code 
++    Vectric machine output configuration file   
++                                                
++================================================
++                                                
++ History                                        
++                                                
++ Who        When       What                     
++ ========== ========== =========================
++ Golub Alex 21/10/2017 Written from GCode_mm.pp
++ ===============================================
+
+POST_NAME = "RepRap Marlin gcode (*.g)"
+
+FILE_EXTENSION = "g"
+
+UNITS = "MM"
+
++------------------------------------------------
++    Line terminating characters                 
++------------------------------------------------
+LINE_ENDING = "[13][10]"
+
++------------------------------------------------
++    Block numbering                             
++------------------------------------------------
+LINE_NUMBER_START     = 0
+LINE_NUMBER_INCREMENT = 10
+LINE_NUMBER_MAXIMUM = 999999
+
++================================================
++                                                
++    Formating for variables                     
++                                                
++================================================
+VAR LINE_NUMBER = [N|A|N|1.0]
+VAR SPINDLE_SPEED = [S|A|S|1.0]
+VAR FEED_RATE = [F|C|F|1.1]
+VAR X_POSITION = [X|C|X|1.3]
+VAR Y_POSITION = [Y|C|Y|1.3]
+VAR Z_POSITION = [Z|C|Z|1.3]
+VAR ARC_CENTRE_I_INC_POSITION = [I|A|I|1.3]
+VAR ARC_CENTRE_J_INC_POSITION = [J|A|J|1.3]
+VAR X_HOME_POSITION = [XH|A|X|1.3]
+VAR Y_HOME_POSITION = [YH|A|Y|1.3]
+VAR Z_HOME_POSITION = [ZH|A|Z|1.3]
+ 
++================================================
++                                                
++    Block definitions for toolpath output       
++                                                
++================================================
+
++------------------------------------------------
++  Commands output at the start of the file
++------------------------------------------------
+begin HEADER
+ 
+"G92 X0 Y0 Z0"
+"G90"
+"G0 [ZH]"
+"G0 [XH] [YH] [ZH]"
+ 
++------------------------------------------------
++  Commands output for rapid moves 
++------------------------------------------------
+ 
+begin RAPID_MOVE
+ 
+"G0 [X] [Y] [Z]"
+ 
+ 
++------------------------------------------------
++  Commands output for the first feed rate move
++------------------------------------------------
+ 
+begin FIRST_FEED_MOVE
+ 
+"G1 [X] [Y] [Z] [F]"
+ 
+ 
++------------------------------------------------
++  Commands output for feed rate moves
++------------------------------------------------
+ 
+begin FEED_MOVE
+ 
+"G1 [X] [Y] [Z]"
+ 
+ 
++-------------------------------------------------
++Commands output for the first clockwise arc move
++-------------------------------------------------
+ 
+begin FIRST_CW_ARC_MOVE
+
+"G2 [X] [Y] [I] [J] [F]"
+ 
+ 
++-------------------------------------------------
++  Commands output for clockwise arc  move
++-------------------------------------------------
+ 
+begin CW_ARC_MOVE
+ 
+"G2 [X] [Y] [I] [J]"
+ 
+ 
++-------------------------------------------------
++  Commands output for the first CCW arc move
++-------------------------------------------------
+ 
+begin FIRST_CCW_ARC_MOVE
+ 
+"G3 [X] [Y] [I] [J] [F]"
+ 
+ 
++-------------------------------------------------
++  Commands output for counterclockwise arc  move
++-------------------------------------------------
+ 
+begin CCW_ARC_MOVE
+ 
+"G3 [X] [Y] [I] [J]"
+ 
+ 
++-------------------------------------------------
++  Commands output at the end of the file
++-------------------------------------------------
+ 
+begin FOOTER
+ 
+"G0 [ZH]"
+"G0 [XH] [YH] [ZH]"
